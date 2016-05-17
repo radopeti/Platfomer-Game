40580c86e7dacff18232300c7e63aae02b0b3068
Peter Rado <rado.peti@gmail.com>
2016-05-17 12:12:54 +0200

    Start project
    This project is inspired by Megaman and Udacity How to make a platformer
    using libGDX online course
    (https://www.udacity.com/course/how-to-make-a-platformer-using-libgdx--ud406).

---
+++ html/src/com/platformer/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.platformer.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.platformer.game.PlatformerGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new PlatformerGame();
+        }
+}
